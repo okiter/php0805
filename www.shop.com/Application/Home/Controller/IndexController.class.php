3ced75ed2c9a9ddc1a33e8227c8a179cc5b95803
okiter <guoguanzhao@qq.com>
2015-11-14 10:15:41 +0800

    前台页面重构

--- www.shop.com/Application/Home/Controller/IndexController.class.php
+++ www.shop.com/Application/Home/Controller/IndexController.class.php
@@ -7,6 +7,25 @@ class IndexController extends Controller
 {
     public function index()
     {
-        echo 'www.shop.com';
+
+        $this->assign('isHiddenMenu',false);
+        $this->assign('meta_title','京西商城首页');
+        $this->display('index');
     }
+
+    public function lst()
+    {
+        $this->assign('isHiddenMenu',true);
+        $this->assign('meta_title','商品列表');
+        $this->display('lst');
+    }
+
+    public function show()
+    {
+
+        $this->assign('isHiddenMenu',true);
+        $this->assign('meta_title','京西商城---XXXX商品');
+        $this->display('show');
+    }
+
 }
